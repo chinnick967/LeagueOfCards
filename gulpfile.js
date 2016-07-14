c432096aa2815603b0f4d02adfb4057f804a4eb4
Alexander Sage <sagezander@gmail.com>
2016-07-14 01:05:21 -0700

    Added build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 const gulp = require('gulp');
 const jasmine = require('gulp-jasmine');
 const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
+const gulpIf = require('gulp-if');
+const run = require('run-sequence');
+const watch = require('gulp-watch');
+
+
+var isProd = false;
+
 
 const config = {
 	test: {
@@ -8,6 +18,10 @@ const config = {
 	},
 	sass: {
 		src: 'src/css/index.scss',
+		dest: 'src/'
+	},
+	scripts: {
+		src: ['./scripts.config'].concat(require('./scripts.config')),
 		dest: 'src/'
 	}
 };
@@ -19,14 +33,36 @@ gulp.task('test', function () {
 	});
 });
 
+gulp.task('scripts', function () {
+	
+	return gulp.src(config.scripts.src)
+		.pipe(sourcemaps.init())
+		.pipe(gulpIf(isProd, babel({presets: ['es2015']})))
+		.pipe(concat('bundle.js'))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(config.scripts.dest));
+});
+
 gulp.task('sass', function () {
 	return gulp.src(config.sass.src)
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest(config.sass.dest))
 });
 
-gulp.task('build', ['sass']);
+gulp.task('build', ['sass', 'scripts']);
 
 gulp.task('default', function () {
-	gulp.watch(config.sass.src, ['sass']);
+	watch(['./src/css/*.scss'], function () {
+		run('sass');
+	});
+	watch(['./scripts.config.js', './src/Engine/**/*.js', './src/vendor/*.js'], () => {
+		console.log ('here');
+		
+	    run('scripts');
+	});
+});
+
+gulp.task('prod', function () {
+	isProd = true;
+	run('build');
 });
