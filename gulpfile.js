72e24ad4814f76c647dc1757c7267c7a5f23600e
Alexander Sage <sagezander@gmail.com>
2016-07-26 21:20:38 -0700

    Fixed production build process

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,9 @@ const sourcemaps = require('gulp-sourcemaps');
 const gulpIf = require('gulp-if');
 const run = require('run-sequence');
 const watch = require('gulp-watch');
+const through2 = require('through2');
+const uglify = require('gulp-uglify');
+
 
 
 var isProd = false;
@@ -39,6 +42,15 @@ gulp.task('scripts', function () {
 		.pipe(sourcemaps.init())
 		.pipe(gulpIf(isProd, babel({presets: ['es2015']})))
 		.pipe(concat('bundle.js'))
+		.pipe(wrapIIFE())
+
+		.pipe(gulpIf(isProd, uglify({
+			mangle: true
+		})))
+		.on('error', function () {
+			console.log(arguments);
+		})
+
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest(config.scripts.dest));
 });
@@ -66,3 +78,13 @@ gulp.task('prod', function () {
 	isProd = true;
 	run('build');
 });
+
+function wrapIIFE () {
+	return through2.obj(function (chunk, enc, callback) {
+		var str = chunk.contents.toString();
+		var res = '(function () {' + str.trim().replace(/^"use strict";/g, '') + '}());';
+		chunk.contents = new Buffer(res);
+		this.push(chunk);
+		return callback();
+	})
+}
